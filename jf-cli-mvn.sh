06d44c8de49997972c99882c75e1181832e735e7
Krishna Manchikalapudi <krishnam@jfrog.com>
2024-10-04 15:57:31 -0700

    GitHub actions job

---
+++ jf-cli-mvn.sh
@@ -0,0 +1,52 @@
+# SET meta-data to differentiate application category, such as application or internal-library
+# export PACKAGE_CATEGORIES=(WEBAPP, SERVICE, LIBRARY, BASEIMAGE)
+clear
+# TOKEN SETUP
+# jf c add --user=krishnam --interactive=true --url=https://psazuse.jfrog.io --overwrite=true 
+
+# Config - Artifactory info
+export JF_RT_URL="https://psazuse.jfrog.io" JFROG_NAME="psazuse" JFROG_RT_USER="krishnam" JFROG_CLI_LOG_LEVEL="DEBUG" # JF_ACCESS_TOKEN="<GET_YOUR_OWN_KEY>"
+export RT_REPO_VIRTUAL="krishnam-mvn-virtual"
+
+echo " JFROG_NAME: $JFROG_NAME \n JF_RT_URL: $JF_RT_URL \n JFROG_RT_USER: $JFROG_RT_USER \n JFROG_CLI_LOG_LEVEL: $JFROG_CLI_LOG_LEVEL \n "
+
+# MVN 
+## Config - project
+### CLI
+export BUILD_NAME="spring-petclinic" BUILD_ID="cmd.mvn.$(date '+%Y-%m-%d-%H-%M')" PACKAGE_CATEGORY="WEBAPP" RT_PROJECT_RB_SIGNING_KEY="krishnam"
+
+### Jenkins
+# export BUILD_NAME=${env.JOB_NAME} BUILD_ID=${env.BUILD_ID} PACKAGE_CATEGORY="WEBAPP"
+# References: 
+# https://www.jenkins.io/doc/book/pipeline/jenkinsfile/#using-environment-variables 
+# https://wiki.jenkins.io/JENKINS/Building+a+software+project 
+
+echo " BUILD_NAME: $BUILD_NAME \n BUILD_ID: $BUILD_ID \n JFROG_CLI_LOG_LEVEL: $JFROG_CLI_LOG_LEVEL  \n RT_PROJECT_REPO: $RT_PROJECT_REPO  \n "
+
+jf mvnc --server-id-resolve ${JFROG_NAME} --server-id-deploy ${JFROG_NAME} --repo-resolve-releases ${RT_REPO_VIRTUAL} --repo-resolve-snapshots ${RT_REPO_VIRTUAL} 
+
+
+## Create Build
+echo "\n\n**** MVN: Package ****\n\n" # --scan=true
+jf mvn clean install -DskipTests=true --build-name=${BUILD_NAME} --build-number=${BUILD_ID} --detailed-summary=true 
+
+# setting build properties
+export e_env="e_demo" e_org="e_ps" e_team="e_arch" e_build="maven" e_job="cmd" # These properties were captured in Builds >> spring-petclinic >> version >> Environment tab
+
+
+## bce:build-collect-env - Collect environment variables. Environment variables can be excluded using the build-publish command.
+jf rt bce ${BUILD_NAME} ${BUILD_ID}
+
+## bag:build-add-git - Collect VCS details from git and add them to a build.
+jf rt bag ${BUILD_NAME} ${BUILD_ID}
+
+## bp:build-publish - Publish build info
+echo "\n\n**** Build Info: Publish ****\n\n"
+jf rt bp ${BUILD_NAME} ${BUILD_ID} --detailed-summary=true
+
+# set-props
+echo "\n\n**** Props: set ****\n\n"  # These properties were captured Artifacts >> repo path 'spring-petclinic.---.jar' >> Properties
+jf rt sp "env=demo;job=cmd;org=ps;team=arch;pack_cat=webapp;build=maven;ts=ts-${BUILD_ID}" --build="${BUILD_NAME}/${BUILD_ID}"
+
+
+echo "\n\n**** DONE ****\n\n"
